653ad112e007bcfc8960e427c06644a26855ce27
victor c <victor.coatalem@gmail.com>
2017-02-27 11:27:21 +0100

    up des boutons

--- Assets/HomeMadeScripts/TableCard.cs
+++ Assets/HomeMadeScripts/TableCard.cs
@@ -159,14 +159,6 @@ public class TableCard : MonoBehaviour
             }
             paragraph += str.Substring(index, pas) + '\n';
             index += pas;
-            
-/*
-                while(str[index] != ' ')
-                {
-                    paragraph += str[index];
-                    index++;
-                }
-                */
 
         }
     
@@ -185,10 +177,17 @@ public class TableCard : MonoBehaviour
         c = false;
         d = false;
 
+        Button1.gameObject.SetActive(true);
+        Button2.gameObject.SetActive(true);
+        Button3.gameObject.SetActive(true);
+        Button4.gameObject.SetActive(true);
+
         Button1.enabled = true;
         Button2.enabled = true;
         Button3.enabled = true;
         Button4.enabled = true;
+
+
 
         B1text.text = "";
         B2text.text = "";
@@ -232,15 +231,13 @@ public class TableCard : MonoBehaviour
                             Recit.text = paragraph("Un groupe de voyageurs s'approche de la ville, une occasion pour vous de vous mêler à la foule!");
                             B1text.text = "Continuer";
                             B2text.text = "Poursuivre votre voyage";
-                            Button3.enabled = false;
-                            Button4.enabled = false;
                         }
                     break;
 
                     case "TheGate 11":
                         path = "TheGate 1>";
                         Recit.text = paragraph("Les gardes ne semblent pas d'humeur à tergiverser. Sans doute avez vous interrompu une trépidante partie de cartes ?");
-                        Continuer("Continuer");
+                        B1text.text = "Continuer";
                         break;
 
                     
@@ -251,8 +248,6 @@ public class TableCard : MonoBehaviour
                         B1text.text = "Accepter l'offre (coût : 2 or)";
                         Button1.enabled = s.gold >= 2;
                         B2text.text = "Refuser et poursuivre votre périple";
-                        Button3.enabled = false;
-                        Button4.enabled = false;
 
                         break;
 
@@ -260,7 +255,7 @@ public class TableCard : MonoBehaviour
                         Recit.text = paragraph("Non sans réchigner, vous donnez au garde le prix exigé en soupirant à l'idée qu'il dépense en argent et en filles de joie votre argent si dûrement gagné..");
                         basicReward(-2, 0, 0, 0);
                         path = "TheGate 1#";
-                        Continuer("Continuer");
+                        B1text.text = "Continuer";
                         break;
 
                     case "TheGate 122":
@@ -270,13 +265,13 @@ public class TableCard : MonoBehaviour
                     case "TheGate 13":
                         path = "TheGate 1>";
                         Recit.text = paragraph("Même dans l'embarras où vous êtes plongés, vous vous efforcez de ne pas perdre la face. Vous expliquez posément aux gardes ce qui vous ammène ici, en espérant qu'ils soient recéptifs à vos malheurs.");
-                        Continuer("Continuer");
+                        B1text.text = "Continuer";
 
                         break;
 
                     case "TheGate 14":
                         Recit.text = paragraph("Voyant les gardes s'approcher, vous vous enfuyez à grandes enjambées. Ces derniers ne prennent pas le temps de poursuivre la vermine que vous êtes à leur yeux, et reprennent leur partie de cartes..");
-                        Continuer("Poursuivre votre périple");
+                        B1text.text = "Poursuivre votre périple";
 
                         break;
 
@@ -318,7 +313,7 @@ public class TableCard : MonoBehaviour
                         basicReward(-1, 0, 0, 0);
 
                         path = "TheGate 1#";
-                        Continuer("Entrer dans la ville");
+                        B1text.text = "Entrer dans la ville";
                         return;
 
                     case "TheGate 1>14":
@@ -373,7 +368,7 @@ public class TableCard : MonoBehaviour
                             path += '#';
                             Recit.text = paragraph("Un rongeur, une relève, deux gardes parlant de femmes.. Vous ne ressentez que le froid et l'impression d'avoir attendu pour rien.");
                             basicReward(0, 0, -1, 0);
-                            Continuer("Continuer");
+                            B1text.text = "Continuer";
                         }
                         break;
 
@@ -382,25 +377,25 @@ public class TableCard : MonoBehaviour
                         //jet agi
                         // -> réussite
                         path = "TheGate 1#";
-                        Continuer("Entrer dans la ville");
+                        B1text.text = "Entrer dans la ville";
                         break;
 
                     case "TheGate 32":
                         //jet char
                         // -> réussite
                         path = "TheGate 1#";
-                        Continuer("Entrer dans la ville");
+                        B1text.text = "Entrer dans la ville";
                         break;
 
                     case "TheGate 33":
                         Recit.text = paragraph("Votre méfait accompli, vous pillez le cadavre encore chaud du marchand, et prenez la fuite avant que la caravane immobile attire l'attention des gardes..");
                         basicReward(6, 0, 0, -4);
-                        Continuer("Continuer");
+                        B1text.text = "Continuer";
                         break;
 
                     case "TheGate 34":
                         Recit.text = paragraph("Le marchand remet sa route en place et reprend son chemin. Vous avez manqué l'occasion.. Il est temps de reprendre votre voyage");
-                        Continuer("Poursuivre votre périple");
+                        B1text.text = "Poursuivre votre périple";
 
                         break;
 
@@ -431,7 +426,7 @@ public class TableCard : MonoBehaviour
                         Recit.text = paragraph("L'alcool vient réchauffer votre sang durement éprouvé, vous voyez dans l'écume des pintes le reflet joyeux de vos dernières aventures.");
                         basicReward(-3, 0, 50, 10);
 
-                        Continuer("Sortir de la ville et poursuivre votre voyage");
+                        B1text.text = "Sortir de la ville et poursuivre votre voyage";
                         path = "TheGate 1#1>";
 
                         break;
@@ -440,20 +435,20 @@ public class TableCard : MonoBehaviour
                         Recit.text = paragraph("Dans l'intimité de l'autel, vous trouvez le réconfort et une présence bienfaitrice. Est-ce un message des Grands, porteur de fortune ? Seul l'avenir vous le dira.");
                         basicReward(0, 5, 0, 30);
 
-                        Continuer("Sortir de la ville et poursuivre votre voyage");
+                        B1text.text = "Sortir de la ville et poursuivre votre voyage";
                         path = "TheGate 1#1>";
 
                         break;
 
                     case "TheGate 1#13":
                         Recit.text = paragraph("Assis à l'abri sous l'arche d'une poterne, vous entreprenez de confectionner un pansement pour couvrir vos plaies. ");
-                        Continuer("Sortir de la ville et poursuivre votre voyage");
+                        B1text.text = "Sortir de la ville et poursuivre votre voyage";
                         path = "TheGate 1#1>";
                         break;
 
                     case "TheGate 1#14":
                         Recit.text = paragraph("L'armurier vous laisse utiliser sa meule par vous même. Vous vous donnez à coeur joie de ranimer le tranchant des beaux jours de votre arme.");
-                        Continuer("Sortir de la ville et poursuivre votre voyage");
+                        B1text.text = "Sortir de la ville et poursuivre votre voyage";
 
                         path = "TheGate 1#1>";
                         break;
@@ -477,12 +472,12 @@ public class TableCard : MonoBehaviour
                 {
                     case "Gobelins! ":
                         Recit.text = paragraph("Une horde de gobelins vous attaque! Aux armes !");
-                        Continuer("Combattre!");
+                        B1text.text = "Combattre!";
                         break;
                     case "Gobelins! 1":
                         Recit.text = paragraph("Victorieux des créatures infâmes, vous finissez de piller les dépouilles, espérant trouver quelques écus entre deux colifichets d'os et et de silex.");
                         basicReward(2, 0, 0, 0);
-                        Continuer("Reprendre votre périple");
+                        B1text.text = "Reprendre votre périple";
 
                         break;
 
@@ -520,27 +515,27 @@ public class TableCard : MonoBehaviour
                     case "LaSource 11":
                         Recit.text = paragraph("De retour à la surface, vous voyez un bandit s'en prenant à votre bivouac. Vous voyant de retour, il dégaine un coutelas et vous charge comme un bélier. ");
 
-                        Continuer("Se défendre!");
+                        B1text.text = "Se défendre!";
                         break;
 
                     case "LaSource 111":
                         Recit.text = paragraph("La vermine terassée, vous recupérer quelques babioles de sa bourse, qui vous l'esperez auront quelque valeur que ce soit aux yeux d'un marchand");
                         basicReward(2, 0, 0, 0);
 
-                        Continuer("Poursuivre votre périple");
+                        B1text.text = "Poursuivre votre périple";
                         path = "LaSource 121";
                         break;
 
                     case "LaSource 12":
                         Recit.text = paragraph("A chaque mètre que vous parcourez il vous semble que le trésor est un peu plus loin.. Vous devez vous rendre à l'évidence, vous ne parviendez pas au but, il est temps de faire demi-tour..");
 
-                        Continuer("Remonter à la surface..");
+                        B1text.text = "Remonter à la surface..";
                         break;
 
                     case "LaSource 121":
                         Recit.text = paragraph("De retour à la surface, vous n'avez pas le temps de reprendre votre souffle que vous constatez que votre bivouac a été pillé.. Animaux ? Bandits ? Qui sait. Pour l'heure, vous devez reprendre votre route.");
 
-                        Continuer("Poursuivre votre périple");
+                        B1text.text = "Poursuivre votre périple";
                         break;
 
                     case "LaSource 1211":
@@ -550,7 +545,7 @@ public class TableCard : MonoBehaviour
                     case "LaSource 2":
                         Recit.text = paragraph("Le fond de l'air est doux et le bruit blanc de l'eau baigne les lieux dans une tranquilité infinie. Ressourcé, vous reprenez votre chemin.");
 
-                        Continuer("Reprendre votre périple");
+                        B1text.text = "Reprendre votre périple";
                         break;
 
                     case "LaSource 21":
@@ -559,7 +554,7 @@ public class TableCard : MonoBehaviour
 
                     case "LaSource 3":
                         Recit.text = paragraph("Fraîche et amère, l'eau vient rincer vos entrailles assechées par l'effort. Le liquide coule désormais dans vos veines, et vous emplie d'une force nouvelle. Il est temps de reprendre votre périple !");
-                        Continuer("Reprendre votre route");
+                        B1text.text = "Reprendre votre route";
                         break;
 
                     case "LaSource 31":
@@ -567,12 +562,10 @@ public class TableCard : MonoBehaviour
                         return;
 
                     case "LaSource 4":
-                        Recit.text = paragraph("Poursuivre votre périple");
+                        Endcard(false);
                         break;
 
-                    case "LaSource 41":
-                        Endcard(true);
-                        return;
+                   
 
                 }
                 break;
@@ -584,12 +577,12 @@ public class TableCard : MonoBehaviour
                     case "HordeSquelette ":
                         Recit.text = paragraph("Le Fléau s'est déja étendu sur ses terres. L'air empeste la mort et les seules plantes poussant ici ne survivent que grace à la peste, charriant les dépouilles torturées.. Alors que vous vous efforcez de quitter les lieux au plus vite, une horde de squelettes animés vous attaque!");
 
-                        Continuer("Combattre!");
+                        B1text.text = "Combattre!";
                         break;
 
                     case "HordeSquelette 1":
                         Recit.text = paragraph("Les os des squelettes redeviennent poussières, et leurs armes s'ensevelissent sous la terre. Vous sentez une présence s'élever vers le ciel, et le vent vous apporte des murmures d'outretombe: \"Merci.\"");
-                        Continuer("Reprendre votre périple");
+                        B1text.text = "Reprendre votre périple";
                         break;
 
                     case "HordeSquelette 11":
@@ -645,7 +638,7 @@ public class TableCard : MonoBehaviour
                     case "Silhouette 12":
                         //jet agi
                         Recit.text = paragraph("A quatres pattes dans la terre vous passez une heure ou deux à traquer la bête. Butin de chasse: quelques rongeurs, quelques insectes, de quoi survivre une ou deux nuits de plus..");
-                        Continuer("Sortir de la forêt et reprendre votre voyage");
+                        B1text.text = "Sortir de la forêt et reprendre votre voyage";
                         basicReward(0, 0, 5, 0);
                         break;
 
@@ -655,7 +648,7 @@ public class TableCard : MonoBehaviour
 
                     case "Silhouette 13":
                         Recit.text = paragraph("Vous vous asseyez sur une buche à l'ombre d'un grand chêne ou vous appreciez cette matinée ensoleillée. Un vent d'ouest vient carresser votre joue , emplissant vos narines d'odeurs d'érable et de fenouil. Il est l'heure de reprendre votre voyage!");
-                        Continuer("Sortir de la forêt et reprendre votre voyage");
+                        B1text.text = "Sortir de la forêt et reprendre votre voyage";
                         basicReward(0, 2, 0, 0);
                         
                         break;
@@ -666,7 +659,7 @@ public class TableCard : MonoBehaviour
 
                     case "Silhouette 14":
                         Recit.text = "Essouflé et las d'avoir couru en vain, vous commencez à rebrousser chemin. Vous sentez une grande aggressivité dans les lieux. Les arbres se racornissent et les racines se ploient. Vous pressez le pas avant que les choses prennent une tournure autrement plus inquiétante..";
-                        Continuer("Sortir de la forêt et reprendre votre voyage");
+                        B1text.text = "Sortir de la forêt et reprendre votre voyage";
                         basicReward(0, 0, 0, -2);
                         break;
 
@@ -692,7 +685,7 @@ public class TableCard : MonoBehaviour
 
                     case "LeCol 1":
                         Recit.text = paragraph("La traversée dure deux jours est en semée d'embûches. A plusieurs reprises vous échappez à des avalanches et éboulements qui auraient eu raison de moins chanceux que vous. Vous arrivez finalement de l'autre coté, extenué, mais heureux d'avoir fini la traversée.");
-                        Continuer("Poursuivre votre périple");
+                        B1text.text = "Poursuivre votre périple";
                         basicReward(0, 0, -10, 0);
 
                         break;
@@ -726,7 +719,7 @@ public class TableCard : MonoBehaviour
                     case "LaDame 1":
                         Recit.text = paragraph("La dame vous voit et un sourire s'esquisse sur son visage. Sans dire un mot, elle vous donne l'épée et, les yeux pleins de mélancolie, elle se laisse tomber dans le lac qui, par quelque sortilège, se recouvre de ronces, de roses et de chardon.");
                         //ajouter une épée.
-                        Continuer("Poursuivre votre périplé");
+                        B1text.text = "Poursuivre votre périple";
                         break;
 
                     case "LaDame 2":
@@ -739,7 +732,13 @@ public class TableCard : MonoBehaviour
                 }
                 break;
         }
-        
+
+        ButtonDisplay(Button1);
+        ButtonDisplay(Button2);
+        ButtonDisplay(Button3);
+        ButtonDisplay(Button4);
+
+
         clicked = false;
         StartCoroutine(WaitForClick());
 
@@ -948,6 +947,27 @@ public class TableCard : MonoBehaviour
     public void DemiTour()
     {
         Player.transform.position = new Vector3(s.lastposx, 1, s.lastposz);
+    }
+
+
+    public void ButtonDisplay(Button b)
+    {
+        Text t = b.GetComponentInChildren<Text>();
+        if (t.text == "")
+        {
+            b.gameObject.SetActive(false);
+        }
+        else if (b.enabled == false)
+        {
+            b.image.color = Color.gray;
+        }
+        else if (b.enabled == true)
+        {
+
+            b.image.color = Color.white;
+        }
+
+
     }
 
 
