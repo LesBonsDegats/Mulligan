b9c7a400b10a0d1ff2f0e6d4a337625331354c13
EMMANUEL BOURDET <EMMANUEL BOURDET>
2017-02-28 22:59:04 +0100

    debut fiche perso

--- Assets/HomeMadeScripts/textScript.cs
+++ Assets/HomeMadeScripts/textScript.cs
@@ -3,31 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public class textScript : MonoBehaviour {
-   public NewBehaviourScript s;
-    Text vie;
-    Text nourriture;
-    Text or;
-    Text experience;
-    Text moral;
-    Text force;
-    Text Agilité;
-    Text Force;
-    Text Chance;
-    Text Charisme;
-    Text Intelligence;
-    Text nom;
+     public NewBehaviourScript s;
+    public Text vie;
+    public Text nourriture;
+    public Text or;
+    public Text experience;
+    public Text moral;
+    public Text Agilité;
+    public Text Force;
+    public Text Chance;
+    public Text Charisme;
+    public Text Intelligence;
+    public Text nom;
     // Use this for initialization
     void Start () {
-
-
         change_text();
 	}
 	public void change_text()
     {
-        vie.text = "vie: " + s.life.ToString();
-        nourriture.text = "nourriture:" + s.hunger.ToString();
-        or.text = "or: " + s.gold.ToString();
-        moral.text = "moral: " + s.moral.ToString();
+        vie.text = "Vie : " + s.life.ToString() + "/" + s.lifemax.ToString(); ;
+        nourriture.text = "Nourriture :" + s.hunger.ToString() +"/" + s.hungermax.ToString(); ;
+        or.text = "Or : " + s.gold.ToString();
+        moral.text = "Moral: " + s.moral.ToString() + "/" + s.moralmax.ToString();
+        experience.text = "Experience:  " + s.xp.ToString() + "/" + s.xpmax.ToString();
+        Force.text = "Force:  " + s.strenght.ToString();
+        Chance.text = "Chance:  " + s.luck.ToString();
+        Charisme.text = "Charisme:  " + s.charisma.ToString();
+        Agilité.text = "Agilité:  " + s.agility.ToString();
+        Intelligence.text = "Intelligence:  " + s.intel.ToString();
+        nom.text = s.name;
     } 
 	// Update is called once per frame
 	void Update () {
