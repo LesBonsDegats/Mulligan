9d4fc0faf32e11d1c3f3e4e4f2c878dd45cdfbc5
EMMANUEL BOURDET <EMMANUEL BOURDET>
2017-03-01 19:55:40 +0100

    test photon

---
+++ Assets/HomeMadeScripts/GUIControler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GUIControler : MonoBehaviour {
+    Text statustext;
+	// Use this for initialization
+	void Start () {
+        statustext = GameObject.Find("statustext").GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        statustext.text = "Status: " + PhotonNetwork.connectionStateDetailed.ToString();
+
+    }
+}
---
+++ Assets/HomeMadeScripts/NetworkControler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkControler : MonoBehaviour
+{
+    private string gameversion = "0.1";
+    // Use this for initialization
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings(gameversion);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("Status: " + PhotonNetwork.connectionStateDetailed.ToString());
+    }
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join the room");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("JoinedRoom");
+    }
+}
