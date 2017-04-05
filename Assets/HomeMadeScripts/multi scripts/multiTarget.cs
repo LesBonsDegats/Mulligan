8a825b348217e260d6c2e3f743f90c13a6baffe6
project-epita <bourdet.jeremie@gmail.com>
2017-04-05 16:06:43 +0200

    targetmulti

---
+++ Assets/HomeMadeScripts/multi scripts/multiTarget.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class multiTarget : MonoBehaviour {
+
+    public Collider weapon;
+    public fightcontroller f;
+
+    public int life;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == weapon && f.isAttacking)
+        {
+            f.weapon.enabled = false;
+            bool isDead = loseLife(10);
+            if (isDead)
+            {
+                this.gameObject.SetActive(false);
+            }
+
+            this.transform.Translate(new Vector3(0, 0, -2));
+
+        }
+    }
+
+    private bool loseLife(int dmg)
+    {
+        life -= dmg;
+
+        return (life < 0);
+    }
+}
